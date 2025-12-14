fce77b768584c0fbd8ff5820ab989d2d970cbc2d
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-12-14 22:31:21 +0100

    Refactor Program.cs for cleaner service configuration

--- Program.cs
+++ Program.cs
@@ -34,15 +34,13 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("AllowFrontend"); 
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
 
 
 
