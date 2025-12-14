e3f0ebdee80dd118efafc886401e782da35e4a8b
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-12-14 16:22:16 +0100

    Refactor Program.cs for improved readability

--- Program.cs
+++ Program.cs
@@ -39,14 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 app.UseCors("AllowFrontend"); 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
 
 
 
